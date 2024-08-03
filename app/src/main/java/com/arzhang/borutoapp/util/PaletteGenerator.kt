a6a45ec5944e61307eedde93ca978e0da720e822
arjang_ap <arjang.ap@gmail.com>
2024-08-03 12:17:42 +0330

    Create PaletteGenerator object.

--- app/src/main/java/com/arzhang/borutoapp/util/PaletteGenerator.kt
+++ app/src/main/java/com/arzhang/borutoapp/util/PaletteGenerator.kt
@@ -3,6 +3,7 @@ package com.arzhang.borutoapp.util
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import androidx.palette.graphics.Palette
 import coil.ImageLoader
 import coil.request.ImageRequest
 import coil.request.SuccessResult
@@ -24,6 +25,38 @@ object PaletteGenerator {
             (imageResult.drawable as BitmapDrawable).bitmap
         } else {
             null
+        }
+    }
+
+    fun extractColorsFromBitmap(bitmap: Bitmap) : Map<String, String> {
+        return mapOf(
+            "vibrant" to parseColorSwatch(
+                color = Palette.from(bitmap).generate().vibrantSwatch
+            ),
+            "darkVibrant" to parseColorSwatch(
+                color = Palette.from(bitmap).generate().darkVibrantSwatch
+            ),
+            "onDarkVibrant" to parseBodyColor(
+                color = Palette.from(bitmap).generate().darkVibrantSwatch?.bodyTextColor
+            ),
+        )
+    }
+
+    private fun parseColorSwatch(color: Palette.Swatch?) : String {
+        return if(color != null) {
+            val parsedColor = Integer.toHexString(color.rgb)
+            return "#$parsedColor"
+        } else {
+            "000000"
+        }
+    }
+
+    private fun parseBodyColor(color: Int?): String {
+        return if(color != null) {
+            val parseColor = Integer.toHexString(color)
+            return "#$color"
+        } else {
+            "#FFFFFF"
         }
     }
 
